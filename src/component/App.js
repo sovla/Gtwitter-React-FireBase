408516c48b347510fee5f362528112953de1d45c
sovla <sovla@naver.com>
2021-08-26 08:42:47 +0900

    #4.0 user Log In Out

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "component/Router";
 import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
 
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing.."}
       <footer>&copy; {new Date().getFullYear()}Gwitter</footer>
     </>
   );
--- src/fbase.js
+++ src/fbase.js
@@ -2,13 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTO_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyC354fTkn4FzouHEodJK1Wp8uv2cduykrw",
+  authDomain: "gwitter-e2519.firebaseapp.com",
+  projectId: "gwitter-e2519",
+  storageBucket: "gwitter-e2519.appspot.com",
+  messagingSenderId: "656420529889",
+  appId: "1:656420529889:web:15409148998d0d0058f9a0",
 };
+
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
 export const authService = firebase.auth();
